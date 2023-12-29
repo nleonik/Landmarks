3679fa7cdcc3b4d12bbf51e58983ccd3fe6b29bf
nleonik <npl4560@gmail.com>
2023-12-29 08:51:22 -0600

    testing remote github repository connection

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -32,7 +32,7 @@ struct LandmarkDetail: View {
                     .font(.title)
                     .foregroundColor(.green)
                     FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
-  
+                       
                 }
                 HStack {
                     Text(landmark.park)
