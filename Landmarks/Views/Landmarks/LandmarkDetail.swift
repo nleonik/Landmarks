e7af636bbea6d609576eba245daf1b41326ffae9
nleonik <npl4560@gmail.com>
2023-12-29 08:31:48 -0600

    added favorites pre shapes

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
     
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
     var body: some View {
+       @Bindable var modelData = modelData
+        
+        
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
@@ -20,10 +27,13 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
+                HStack {
+                  Text(landmark.name)
                     .font(.title)
                     .foregroundColor(.green)
-                
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+  
+                }
                 HStack {
                     Text(landmark.park)
                     Spacer()
@@ -47,5 +57,7 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: ModelData().landmarks[0])
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: modelData.landmarks[0])
+        .environment(modelData)
 }
